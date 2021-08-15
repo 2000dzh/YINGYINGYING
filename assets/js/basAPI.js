7868729467432593cfdd0eabc9ea489a8e27d96e
yiquanyigeyinyingguai <2196714064@qq.com>
2021-08-15 17:52:48 +0800

    首页主体模块

--- assets/js/basAPI.js
+++ assets/js/basAPI.js
@@ -1,4 +1,11 @@
 
-    $.ajaxPrefilter(function (options) {
-        options.url = 'http://www.liulongbin.top:3008' + options.url
-    })
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://www.liulongbin.top:3008' + options.url;
+
+
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token") || ''
+        };
+    }
+})
---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+$(function () {
+    //调用 getUserInfo 获取用户的基本信息
+    getUserInfo()
+
+    $("#btnLogout").on('click', function () {
+        layer.confirm('确定要退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token');
+
+            location.href = './login.html'
+
+            layer.close(index);
+        });
+    })
+})
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        //headers 就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem("token") || ''
+        // },
+        success: function (res) {
+            console.log(res);
+            if (res.code !== 0) return layui.layer.msg(res.message)
+
+
+
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    let temp = user.nickname || user.id;
+    $("#welcome").text('欢迎' + temp)
+    if (user.user_pic) {
+        $(".layui-nav-img").attr('src', user.user_pic).show()
+        $(".text-avatar").hide()
+    } else {
+        $(".layui-nav-img").hide()
+
+        let n_temp = temp[0].toUpperCase()
+        $(".text-avatar").text(n_temp).show()
+    }
+}
